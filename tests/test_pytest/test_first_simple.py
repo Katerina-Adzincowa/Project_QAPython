24ad78c3841404d995c96f901600de39371da299
Katerina-Adzincowa <adzintsovakaterina@gmail.com>
2026-02-13 23:14:57 +0100

    Add first tests
    (cherry picked from commit ad6b09ec94e4390245ce8ea2b1b389a4918ec2b6)

--- tests/test_pytest/test_first_simple.py
+++ tests/test_pytest/test_first_simple.py
@@ -8,7 +8,6 @@ LABCARDS = f"{BASE_URL}/automation-lab/cards"
 
 def test_open_labcards():
     opts = Options()
-    #opts.add_argument("--headless=new")
     opts.add_argument("--window-size=1980,1600")
     driver = webdriver.Chrome(options=opts)
 
@@ -16,6 +15,5 @@ def test_open_labcards():
 
     assert driver.title == "Task Management Board"
     assert driver.current_url == LABCARDS
-    breakpoint()
     button_view_task_section = driver.find_element(By.CSS_SELECTOR, ".btn-primary")
     assert button_view_task_section.is_displayed()
